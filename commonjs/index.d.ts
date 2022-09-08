359204fca792ddba21acb39bc67b55da5af0b8d0
mgn901 <mgn901+dev@gmail.com>
2022-09-08 12:04:57 +0900

    Clean up commonjs directory.

--- commonjs/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { dateToUnixTime, dateToUnixTimeMillis, isUnixTime, isUnixTimeMillis, UnixTime, UnixTimeMillis, unixTimeMillisToDate, unixTimeMillisToUnixTime, unixTimeToDate, unixTimeToUnixTimeMillis } from './Date';
-import { Nominal } from './Nominal';
-import { isRecord, isTypedArray } from './utilsForTypeGuard';
-export { dateToUnixTime, dateToUnixTimeMillis, isUnixTime, isUnixTimeMillis, UnixTime, UnixTimeMillis, unixTimeMillisToDate, unixTimeMillisToUnixTime, unixTimeToDate, unixTimeToUnixTimeMillis, Nominal, isRecord, isTypedArray, };
