15fac8ba3542a4b1c5d0a57abb7b9a462d63c17a
mgn901 <mgn901+dev@gmail.com>
2026-03-31 02:16:58 +0900

    add: RangeIterator

--- src/range.test.ts
+++ src/range.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { range } from './range.js';
+import { RangeIterator, range } from './range.js';
 
 describe('range', () => {
   test('returns an array of numbers from start to end (exclusive) with a given step', () => {
@@ -10,5 +10,25 @@ describe('range', () => {
     expect(range(-10, -5)).toEqual([-10, -9, -8, -7, -6]);
     expect(range(-10.5, -5.5)).toEqual([-10.5, -9.5, -8.5, -7.5, -6.5]);
     expect(range(0.1, 0.3)).toEqual([0.1]);
+  });
+});
+
+describe('RangeIterator', () => {
+  test('iterates over a range of numbers from start to end (exclusive) with a given step', () => {
+    expect(Array.from(new RangeIterator(0, 10))).toEqual([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    ]);
+    expect(Array.from(new RangeIterator(0, 10, 2))).toEqual([0, 2, 4, 6, 8]);
+    expect(Array.from(new RangeIterator(5, 15))).toEqual([
+      5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+    ]);
+    expect(Array.from(new RangeIterator(5, 15, 3))).toEqual([5, 8, 11, 14]);
+    expect(Array.from(new RangeIterator(-10, -5))).toEqual([
+      -10, -9, -8, -7, -6,
+    ]);
+    expect(Array.from(new RangeIterator(-10.5, -5.5))).toEqual([
+      -10.5, -9.5, -8.5, -7.5, -6.5,
+    ]);
+    expect(Array.from(new RangeIterator(0.1, 0.3))).toEqual([0.1]);
   });
 });
--- src/range.ts
+++ src/range.ts
@@ -5,3 +5,40 @@ export const range = (start: number, end: number, step = 1) =>
   [...Array(Math.ceil((end - start) / step)).keys()].map(
     (_, index) => start + index * step,
   );
+
+const sCurrent = Symbol('current');
+const sEnd = Symbol('end');
+const sStep = Symbol('step');
+
+export class RangeIterator implements IterableIterator<number> {
+  private [sCurrent]: number;
+  private [sEnd]: number;
+  private [sStep]: number;
+
+  constructor(start: number, end: number, step = 1) {
+    this[sCurrent] = start;
+    this[sEnd] = end;
+    this[sStep] = step;
+  }
+
+  next(...[_value]: [] | [unknown]): IteratorResult<number, undefined> {
+    if (this[sCurrent] >= this[sEnd]) {
+      return { done: true, value: undefined };
+    }
+    const value = this[sCurrent];
+    this[sCurrent] += this[sStep];
+    return { done: false, value };
+  }
+
+  return(_value?: unknown): IteratorResult<number, undefined> {
+    return { done: true, value: undefined };
+  }
+
+  throw(_e?: unknown): IteratorResult<number, undefined> {
+    return { done: true, value: undefined };
+  }
+
+  [Symbol.iterator](): this {
+    return this;
+  }
+}
